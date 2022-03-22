ead6c4423dca516d0f5472d635fb425b38f7183b
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-22 14:17:25 -0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -18,8 +18,8 @@ if st.button("Detalhar"):
     else: 
         response = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (nome científico ou a qual família pertence, envelope, diametro, se é RNA ou DNA, sua replicação, informações do genoma.), se for sintomas, busque o vírus que mais se aproxima aos sintomas dados na lista e mostre os motivos da escolha:\n\n Bom.",
-  temperature=0.5,
+  prompt="Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (nome científico ou a qual família pertence, envelope, diametro, se é RNA ou DNA, sua replicação, informações do genoma, em qual hospedeiro parasita - animais, planta, bacterias, fungos etc, qual sua classe/tamanho), se for sintomas, escolha o vírus que mais se aproxima aos sintomas dados na lista e mostre os motivos da escolha:\n\n Bom.",
+  temperature=0,
   max_tokens=200,
   top_p=1,
   frequency_penalty=0,
@@ -33,10 +33,10 @@ if st.button("Detalhar"):
             prompt="Lista:\n"+texto_quebrado+"\n'''\nDescreva os vírus da lista:\n\n"+content+"\nCite vírus relacionado e explique: Além desse,",
             temperature=0.7,
             max_tokens=200,
-            top_p=0,
+            top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
             stop=["\n\n"])
         explicação = explicação0.choices[0].text
         st.write(content+".\n\n"+"Além desse,"+explicação)
-        st.write("Você não pode confiar 100% nessa resposta. O programa ainda está em fase de desenvolvimento e pode conter erros.")
+        st.write("[Você não pode confiar 100% nessa resposta. O programa ainda está em fase de desenvolvimento e pode conter erros.]")
