1d3303d348b2009cd1ebf4725246a074da5cf0f1
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-22 08:21:45 -0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -6,7 +6,7 @@ import openai
 openai.api_key = ""
 
 st.title("Labinftec APP - Detalhar")
-texto = st.text_area("Coloque os ítens aqui", "")
+texto = st.text_area("Especifique os sintomas ou algum vírus.", "")
 keyopenai = st.text_input("Coloque a chave da OpenAI aqui", "")
 
 if keyopenai != "":
@@ -19,7 +19,7 @@ if st.button("Detalhar"):
     else: 
         response = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (envelope, diametro, se é RNA ou DNA, sua replicação, informações do genoma.):\n\n Bom.",
+  prompt="Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (nome científico ou a qual família pertence, envelope, diametro, se é RNA ou DNA, sua replicação, informações do genoma.):\n\n Bom.",
   temperature=0.7,
   max_tokens=200,
   top_p=1,
@@ -27,11 +27,11 @@ if st.button("Detalhar"):
   presence_penalty=0,
   stop=["\n\n"]
 )
-        st.write("Detalhes:")
+        st.write("Resultado da classificação:")
         content = response.choices[0].text
         explicação0 = openai.Completion.create(
             engine="text-davinci-002",
-            prompt="Lista:\n"+texto_quebrado+"\n'''\nDescreva os vírus da lista:\n\n"+content+"\nExplique os detalhes técnicos: Bom,",
+            prompt="Lista:\n"+texto_quebrado+"\n'''\nDescreva os vírus da lista:\n\n"+content+"\nCite vírus relacionado e explique: Além desse,",
             temperature=0.7,
             max_tokens=200,
             top_p=1,
@@ -39,7 +39,4 @@ if st.button("Detalhar"):
             presence_penalty=0,
             stop=["\n\n"])
         explicação = explicação0.choices[0].text
-        st.write(content+".\n\nExplicação: "+explicação)
-
-
-
+        st.write(content+".\n\n"Além desse,"+explicação)
