dbbb5859cd1faaf093bbc3eefecac792cf63b082
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-21 22:46:50 -0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -5,7 +5,7 @@ import openai
 
 openai.api_key = ""
 
-st.title("Labinftec APP - Classificador")
+st.title("Labinftec APP - Detalhes")
 texto = st.text_area("Coloque os ítens aqui", "")
 keyopenai = st.text_input("Coloque a chave da OpenAI aqui", "")
 
@@ -13,33 +13,33 @@ if keyopenai != "":
     openai.api_key = keyopenai
 
 texto_quebrado = texto.replace(',', '\n')
-if st.button("Classificar"):
+if st.button("Detalhar"):
     if texto == "":
         st.write("Você precisa inserir os ítens na caixa de texto.")
     else: 
         response = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="Lista:\n"+texto_quebrado+"\n'''\nClassificação do mais perigoso para o menos perigoso na lista:\n\n1.",
+  prompt="Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (envelope, diametro, se é RNA ou DNA etc.):\n\n Bom.",
   temperature=0.7,
-  max_tokens=150,
+  max_tokens=200,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0,
   stop=["\n\n"]
 )
-        st.write("Resultado da classificação:")
+        st.write("Sobre:")
         content = response.choices[0].text
         explicação0 = openai.Completion.create(
             engine="text-davinci-002",
-            prompt="Lista:\n"+texto_quebrado+"\n'''\nClassificação do mais perigoso para o menos perigoso na lista:\n\n1. "+content+"\nExplicação: Bom,",
+            prompt="Lista:\n"+texto_quebrado+"\n'''\nDescreva os vírus da lista:\n\n"+content+"\nExplique os detalhes técnicos: Bom,",
             temperature=0.7,
-            max_tokens=150,
+            max_tokens=200,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
             stop=["\n\n"])
         explicação = explicação0.choices[0].text
-        st.write("1. "+content+".\n\nExplicação: "+explicação)
+        st.write(content+".\n\nExplicação: "+explicação)
 
 
 
